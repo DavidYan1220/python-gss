24f117a6aef89e93b67390b74d2209878f414db2
Ziyi Yan <52399749+DavidYan1220@users.noreply.github.com>
2019-07-11 11:10:26 +0800

    Homework 7

---
+++ player1.py
@@ -0,0 +1,54 @@
+player = {}
+menu = ''
+
+for i in range(1, 6):
+    number = int(input('Enter player %d\'s jersey number: ' % i))
+    rating = int(input('Enter player %d\'s rating: ' % i))
+    player[number] = rating
+
+lst = sorted(list(player.keys()))
+
+print('ROSTER')
+for i in lst:
+    print('Jersey number: %d, Rating: %d' % (i, player[i]))
+
+while menu != 'q':
+    print('MENU')
+    print('a - Add player')
+    print('d - Remove player')
+    print('u - Update player rating')
+    print('r - Output players above a rating')
+    print('o - Output roster')
+    print('q - Quit')
+
+    menu = input('Choose an option: ')
+
+    if menu == 'a':
+        number = int(input('Enter a new player\'s jersey number: '))
+        rating = int(input('Enter the player\'s rating: '))
+        player[number] = rating
+
+    elif menu == 'd':
+        number = int(input('Enter a jersey number: '))
+        del player[number]
+
+    elif menu == 'u':
+        number = int(input('Enter a jersey number: '))
+        rating = int(input('Enter a new rating for player: '))
+        player[number] = rating
+
+    elif menu == 'r':
+        rating = int(input('Enter a rating: '))
+        lst = sorted(list(player.keys()))
+
+        print('Above', rating)
+        for i in lst:
+            if player[i] > rating:
+                print('Jersey number: %d, Rating: %d' % (i, player[i]))
+
+    elif menu == 'o':
+        lst = sorted(list(player.keys()))
+        print('ROSTER')
+        for i in lst:
+            print('Jersey number: %d, Rating: %d' % (i, player[i]))
+
